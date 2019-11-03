1d97a0242a5dd9f583bb2845f8d5e6f610314bd2
Jeon <wmf85276@gmail.com>
2019-11-03 15:36:06 +0900

    UI setting

---
+++ MediaPlayer/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace MediaPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(709, 187);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(79, 70);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(12, 187);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(79, 70);
+            this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Location = new System.Drawing.Point(206, 110);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(381, 233);
+            this.panel3.TabIndex = 2;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.panel5);
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(381, 233);
+            this.panel4.TabIndex = 0;
+            this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
+            // 
+            // panel5
+            // 
+            this.panel5.Location = new System.Drawing.Point(0, 0);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(381, 233);
+            this.panel5.TabIndex = 0;
+            this.panel5.Paint += new System.Windows.Forms.PaintEventHandler(this.panel5_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+    }
+}
+
---
+++ MediaPlayer/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MediaPlayer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            /* 마우스 이벤트 핸들러 */
+            panel1.MouseUp += new MouseEventHandler(panel1_MouseUp);
+            panel1.MouseDown += new MouseEventHandler(panel1_MouseDown);
+            panel2.MouseUp += new MouseEventHandler(panel2_MouseUp);
+            panel2.MouseDown += new MouseEventHandler(panel2_MouseDown);
+        }
+
+        /* 오른쪽 화살표 버튼 이미지 */
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            Image img = Image.FromFile("..\\..\\Resources\\arrow_right.png");  // 패널 이미지 적용(화살표)
+            graphics.DrawImage(img, 0, 0);
+        }
+
+        /* 오른쪽 화살표 Mouse up 처리 */
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine("오른쪽 Mouse up.");
+            //this.Visible = false;
+            //Form2 form2 = new Form2();
+            //form2.Owner = this;
+            //form2.Show();
+            //panel3.Visible = true;
+            System.Diagnostics.Process.Start("..\\..\\Resources\\MediaPlayer.mp4");
+        }
+
+        /* 오른쪽 화살표 Mouse down 처리 */
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine("오른쪽 Mouse down.");
+        }
+
+        /* 왼쪽 화살표 버튼 이미지 */
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            Image img = Image.FromFile("..\\..\\Resources\\arrow_left.png");  // 패널 이미지 적용(화살표)
+            graphics.DrawImage(img, 0, 0);
+        }
+
+        /* 왼쪽 화살표 Mouse up 처리 */
+        private void panel2_MouseUp(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine("왼쪽 화살표 Mouse up.");
+        }
+
+        /* 왼쪽 화살표 Mouse down 처리 */
+        private void panel2_MouseDown(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine("왼쪽 화살표 Mouse down.");
+        }
+
+        /* 1페이지 그림 */
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            Image img = Image.FromFile("..\\..\\Resources\\1page.jpg");
+            graphics.DrawImage(img, 0, 0, panel3.Width, panel3.Height);
+        }
+
+        /* 2페이지 그림 */
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            Image img = Image.FromFile("..\\..\\Resources\\2page.jpg");
+            graphics.DrawImage(img, 0, 0, panel3.Width, panel3.Height);
+
+            panel4.Visible = false;  // 처음에 안 보이게
+        }
+
+        /* 3페이지 그림 */
+        private void panel5_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            Image img = Image.FromFile("..\\..\\Resources\\2page.jpg");
+            graphics.DrawImage(img, 0, 0, panel3.Width, panel3.Height);
+
+            panel5.Visible = false;  // 처음에 안 보이게
+        }
+    }
+}
